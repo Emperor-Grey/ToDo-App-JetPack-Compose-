75bc89f23c8345b04c6c895c3fa558894b6eb131
King Grey <kinggrey9959@gmail.com>
2024-04-30 11:16:55 +0530

    Search bar properly works now

--- app/src/main/java/com/example/to_doapp/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/example/to_doapp/ui/screens/list/ListAppBar.kt
@@ -2,7 +2,8 @@ package com.example.to_doapp.ui.screens.list
 
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -20,13 +21,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
 import com.example.to_doapp.R
@@ -35,6 +42,7 @@ import com.example.to_doapp.data.models.Priority
 import com.example.to_doapp.ui.theme.LARGE_PADDING
 import com.example.to_doapp.ui.theme.ViewModels.SharedViewModel
 import com.example.to_doapp.utils.SearchAppBarState
+import com.example.to_doapp.utils.TrailingIconState
 
 @Composable
 fun ListAppBar(
@@ -42,12 +50,21 @@ fun ListAppBar(
 ) {
     when (searchAppBarState) {
         SearchAppBarState.CLOSED -> {
-            DefaultListAppbar(onSearchClicked = {}, onSortClicked = {})
+            DefaultListAppbar(onSearchClicked = {
+                sharedViewModel.searchAppBarState.value = SearchAppBarState.OPENED
+                sharedViewModel.searchTextState.value = ""
+            }, onSortClicked = {})
 
         }
 
         else -> {
-            SearchAppBar(text = "Search for tasks...", onSearchClicked = {})
+            SearchAppBar(text = searchTextSate, onSearchClicked = {}, onTextChanged = { newText ->
+                sharedViewModel.searchTextState.value = newText
+
+            }, onCloseClicked = {
+                sharedViewModel.searchAppBarState.value = SearchAppBarState.CLOSED
+                sharedViewModel.searchTextState.value = ""
+            })
         }
     }
 }
@@ -143,45 +160,61 @@ fun DeleteAllAction(onDeleteClicked: () -> Unit) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchAppBar(
-    text: String, onSearchClicked: (String) -> Unit
+    text: String,
+    onTextChanged: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
 ) {
-    var searchQuery by remember {
-        mutableStateOf("")
-    }
-    var isActive by remember {
-        mutableStateOf(false)
-    }
-    val querySuggestions = remember {
-        mutableStateListOf("Your Search's appear here")
+    val focusRequester = remember { FocusRequester() }
+    var trailingIconState by remember {
+        mutableStateOf(TrailingIconState.READY_TO_DELETE)
     }
 
-    SearchBar(modifier = Modifier.fillMaxWidth(),
-        query = searchQuery,
-        onQueryChange = { searchQuery = it },
-        onSearch = {
-            querySuggestions.add(it)
-            isActive = false
-            onSearchClicked(it)
-            searchQuery = ""
-        },
-        placeholder = { Text(text = text) },
-        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Search") },
-        trailingIcon = {
-            if (isActive) {
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .semantics { isTraversalGroup = true }) {
+        SearchBar(modifier = Modifier
+            .align(Alignment.TopCenter)
+            .focusRequester(focusRequester)
+            .semantics { traversalIndex = -1f },
+            query = text,
+            onQueryChange = onTextChanged,
+            onSearch = {
+                onSearchClicked(it)
+            },
+            placeholder = { Text(text = "Search for a task...") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search, contentDescription = "Search"
+                )
+            },
+            trailingIcon = {
                 Icon(modifier = Modifier.clickable {
-                    if (searchQuery.isNotEmpty()) {
-                        searchQuery = ""
-                    } else {
-                        isActive = false
-                    }
-                }, imageVector = Icons.Default.Close, contentDescription = "Close Icon")
-            }
-        },
-        active = isActive,
-        onActiveChange = {
-            isActive = it
-        }) {
+                    when (trailingIconState) {
+                        TrailingIconState.READY_TO_DELETE -> {
+                            onTextChanged("")
+                            trailingIconState = TrailingIconState.READY_TO_CLOSE
+                        }
 
+                        TrailingIconState.READY_TO_CLOSE -> {
+                            if (text.isNotEmpty()) {
+                                onTextChanged("")
+                            } else {
+                                onCloseClicked()
+                                trailingIconState = TrailingIconState.READY_TO_DELETE
+                            }
+                        }
+                    }
+                }, imageVector = Icons.Default.Close, contentDescription = "Close")
+
+            },
+            active = true,
+            onActiveChange = {}) {}
     }
 }
 
---
+++ app/src/main/java/com/example/to_doapp/utils/TrailingIconState.kt
@@ -0,0 +1,5 @@
+package com.example.to_doapp.utils
+
+enum class TrailingIconState {
+    READY_TO_DELETE, READY_TO_CLOSE
+}
